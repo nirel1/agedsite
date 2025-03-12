828a3f8cb2e1d76e7b0a0fd87605c39e2d21984c
NTablang <nathantablang@yahoo.com>
2025-03-12 16:22:01 -0400

    more

--- app/_components/subsections/NewPage.tsx
+++ app/_components/subsections/NewPage.tsx
@@ -1,14 +1,8 @@
-import { Playfair_Display } from 'next/font/google'
 import React from 'react'
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
 
 function NewPage() {
   return (
-    <div className={`flex flex-col  h-screen bg-black ${playfair.className}`}>
+    <div className="flex flex-col h-screen bg-black font-['Times_New_Roman',serif]">
       <div className="text-2xl font-regular text-white">contact@strobe.org</div>
     </div>
   );
